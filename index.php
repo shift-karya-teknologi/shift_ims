cfbb0542925906b3d72bd236835de41ab64b02b4
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-02-21 10:57:17 +0700

    update base url

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ define('TEMPLATE_PATH', __DIR__ . '/#/templates');
 define('TMP_PATH'     , __DIR__ . '/#/tmp');
 
 // url definitions
-define('BASE_URL'    , '/shift.ims');
+define('BASE_URL'    , '/shift_ims');
 define('CSS_BASE_URL', BASE_URL . '/-/css/');
 define('JS_BASE_URL' , BASE_URL . '/-/js/');
 define('LOGIN_URL'   , BASE_URL . '/login');
