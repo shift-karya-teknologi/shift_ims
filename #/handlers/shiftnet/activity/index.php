bee9e9e57946b60a6677565e08c5593178b3f5fd
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-02-23 22:30:18 +0700

    Added activity filter by groupId and clientId

--- #/handlers/shiftnet/activity/index.php
+++ #/handlers/shiftnet/activity/index.php
@@ -1,6 +1,24 @@
 <?php
 
+$clients = $db->query('select id from shiftnet_clients order by id asc')->fetchAll(PDO::FETCH_OBJ);
+$groups = [
+  1 => 'Administrator',
+  2 => 'Member',
+  3 => 'Tamu',
+];
 $str = filter_input(INPUT_GET, 'date', FILTER_SANITIZE_STRING);
+$groupId  = (int)filter_input(INPUT_GET, 'groupId', FILTER_VALIDATE_INT);
+$clientId = (int)filter_input(INPUT_GET, 'clientId', FILTER_VALIDATE_INT);
+$where = '';
+
+if (!array_key_exists($groupId, $groups))
+  $groupId = 0;
+
+if ($groupId != 0)
+  $where .= ' and groupId='.$groupId;
+
+if ($clientId)
+  $where .= ' and clientId='.$clientId;
 
 if (!preg_match("/^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{4}$/", $str)) {
   $str = 'now';
@@ -17,18 +35,22 @@ $endDatetime   = get_shift_end($datetime);
 $q = $db->prepare('select a.*'
   . ' from shiftnet_activities a'
   . ' where (a.dateTime >= :start and a.dateTime <= :end)'
+  . $where
   . ' order by a.dateTime desc');
 $q->bindValue(':start', $startDatetime->format('Y-m-d H:i:s'));
 $q->bindValue(':end'  , $endDatetime->format('Y-m-d H:i:s'));
 $q->execute();
 
 $activities = $q->fetchAll(PDO::FETCH_OBJ);
-
 render('layout', [
   'title'   => 'Aktifitas',
   'sidenav' => render('shiftnet/sidenav', true),
   'content' => render('shiftnet/activity/list', [
     'datetime'   => $datetime,
     'activities' => $activities,
+    'groups' => $groups,
+    'groupId' => $groupId,
+    'clients' => $clients,
+    'clientId' => $clientId,
   ], true)
 ]);
