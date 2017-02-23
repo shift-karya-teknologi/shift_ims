bfd5d3713a6766be36eb74054566419f65595e5b
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-02-23 13:45:53 +0700

    member last 3 day activities

--- #/handlers/shiftnet/member/view.php
+++ #/handlers/shiftnet/member/view.php
@@ -5,12 +5,22 @@ if (!($id && $member = $db->query("select * from shiftnet_members where id=$id")
   header('Location: ./');
   exit;
 }
+
+$datetime = new DateTime();
+$datetime->sub(new DateInterval('P3D'));
+$datetime = $datetime->format('Y-m-d H:i:s');
+
+$activities = $db->query("select *"
+  . " from shiftnet_activities"
+  . " where memberId=$id and dateTime >= '$datetime'")
+  ->fetchAll(PDO::FETCH_OBJ);
   
 render('layout', [
   'title'    => 'Rincian Member',
   'sidenav'  => render('shiftnet/sidenav', true),
   'headnav'  => render('shiftnet/member/view--menu', ['id' => $member->id ], true),
   'content'  => render('shiftnet/member/view', [
-    'member' => $member
+    'member'     => $member,
+    'activities' => $activities,
   ], true)
 ]);
