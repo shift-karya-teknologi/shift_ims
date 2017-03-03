d343e2c5f8f8d9056319eab7a6d6988bb44f23c0
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-03-03 16:28:21 +0700

    prototipe daftar penyesuaian stok

---
+++ #/handlers/pos/stock-adjustment/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$status = isset($_GET['status']) ? (int)$_GET['status'] : -1;
+$sql = 'select * from stock_adjustments where ';
+
+if ($status <= -1)
+  $sql .= '1';
+else
+  $sql .= 'status=:status';
+
+$q = $db->prepare($sql);
+
+if ($status >= 0) {
+  $q->bindValue(':status', $status);
+}
+
+$q->execute();
+
+$items = $q->fetchAll(PDO::FETCH_OBJ);
+
+render('layout', [
+  'title'   => 'Penyesuaian Stok',
+  'headnav' => '
+    <a href="./add" class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+      <i class="material-icons">add</i>
+    </a>',
+  'sidenav' => render('pos/sidenav', true),
+  'content' => render('pos/stock-adjustment/list', [
+    'status' => $status,
+    'items'  => $items
+  ], true),
+]);
