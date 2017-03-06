a99185eb331b55d817996cfef16d2e5c4dd7bdc7
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-03-06 11:59:02 +0700

    update link

--- #/handlers/pos/product/index.php
+++ #/handlers/pos/product/index.php
@@ -2,13 +2,13 @@
 
 require_once CORELIB_PATH . '/Product.php';
 
-$products = $db->query('select * from products order by name asc')
+$products = $db->query('select * from products where type <= 200 order by name asc')
   ->fetchAll(PDO::FETCH_CLASS, Product::class);
 
 render('layout', [
   'title'   => 'Produk',
   'headnav' => '
-    <a href="./add" class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+    <a href="./editor" class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
       <i class="material-icons">add</i>
     </a>',
   'sidenav' => render('pos/sidenav', true),
