7969ba51d2fd5b3c6bc5ba7a97ba652806e05119
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-03-10 22:15:54 +0700

    - change product chooser from select to data list - handle duplicate product

--- #/handlers/pos/purchasing-order/item-editor.php
+++ #/handlers/pos/purchasing-order/item-editor.php
@@ -28,14 +28,8 @@ if (!$order || $order->status != 0) {
   exit;
 }
 
-$costByProductIds = [];
-$products = [];
-$q = $db->query('select id, name, cost from products where active=1 and type=0 order by name asc');
-while ($r = $q->fetchObject()) {
-  $products[] = $r;
-  $productByIds[$r->id] = $r;
-  $costByProductIds[$r->id] = $r->cost;
-}
+$products = $db->query('select id, name, cost from products where active=1 and type=0 order by name asc')->fetchAll(PDO::FETCH_OBJ);
+$errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $action = $_POST['action'];
@@ -51,42 +45,71 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   }
   
   $item->productId = (int)$_POST['productId'];
-  $item->quantity = str_replace('.', '', (string)$_POST['quantity']);
-  $item->cost = str_replace('.', '', (string)$_POST['cost']);
-  $db->beginTransaction();
-  if (!$item->id) {
-    $q = $db->prepare('insert into purchasing_order_details'
-      . ' ( parentId, productId, quantity, cost, subtotalCost)'
-      . ' values '
-      . ' (:parentId,:productId,:quantity,:cost,:subtotalCost)');
-    $q->bindValue(':parentId', $item->parentId);
+  $item->quantity = (int)str_replace('.', '', (string)$_POST['quantity']);
+  $item->cost = (int)str_replace('.', '', (string)$_POST['cost']);
+  
+  if ($item->productId === 0) {
+    $errors['productId'] = 'Anda belum memilih produk.';
   }
   else {
-    $q = $db->prepare('update purchasing_order_details set'
-      . ' productId=:productId,'
-      . ' quantity=:quantity,'
-      . ' cost=:cost,'
-      . ' subtotalCost=:subtotalCost'
-      . ' where id=:id');
-    $q->bindValue(':id', $item->id);
+    // cek duplikat
+    $sql = "select count(0) from purchasing_order_details"
+      . " where parentId=$item->parentId and productId=$item->productId";
+          
+    if ($item->id)
+      $sql = " and id<>$item->id";
+    
+    if ($db->query($sql)->fetchColumn() !== 0)
+      $errors['productId'] = 'Produk sudah ada.';
   }
-  $q->bindValue(':productId', $item->productId);
-  $q->bindValue(':quantity', $item->quantity);
-  $q->bindValue(':cost', $item->cost);
-  $q->bindValue(':subtotalCost', $item->cost * $item->quantity);
-  $q->execute();
   
-  update_purchasing_order_subtotal($orderId);
+  if ($item->quantity === 0) {
+    $errors['quantity'] = 'Kwantitas harus diisi.';
+  }
   
-  $db->commit();
+  if ($item->cost === 0) {
+    $errors['quantity'] = 'Harga beli harus diisi.';
+  }
   
-  $_SESSION['FLASH_MESSAGE'] = 'Item telah disimpan';
-  header('Location: ./editor?id='.$orderId);
-  exit;
+  if (empty($errors)) {
+    $db->beginTransaction();
+    if (!$item->id) {
+      $q = $db->prepare('insert into purchasing_order_details'
+        . ' ( parentId, productId, quantity, cost, subtotalCost)'
+        . ' values '
+        . ' (:parentId,:productId,:quantity,:cost,:subtotalCost)');
+      $q->bindValue(':parentId', $item->parentId);
+    }
+    else {
+      $q = $db->prepare('update purchasing_order_details set'
+        . ' productId=:productId,'
+        . ' quantity=:quantity,'
+        . ' cost=:cost,'
+        . ' subtotalCost=:subtotalCost'
+        . ' where id=:id');
+      $q->bindValue(':id', $item->id);
+    }
+    $q->bindValue(':productId', $item->productId);
+    $q->bindValue(':quantity', $item->quantity);
+    $q->bindValue(':cost', $item->cost);
+    $q->bindValue(':subtotalCost', $item->cost * $item->quantity);
+    $q->execute();
+
+    update_purchasing_order_subtotal($orderId);
+
+    $db->commit();
+      
+    $_SESSION['FLASH_MESSAGE'] = 'Item telah disimpan';
+    header('Location: ./editor?id='.$orderId);
+    exit;
+  }
 }
 
+if ($item->productId)
+  $item->productName = $db->query("select name from products where id=$item->productId")->fetchColumn();
+
 render('pos/purchasing-order/item-editor', [
-    'item' => $item,
-    'products' => $products,
-    'costByProductIds' => $costByProductIds,
+  'item' => $item,
+  'products' => $products,
+  'errors' => $errors
 ]);
