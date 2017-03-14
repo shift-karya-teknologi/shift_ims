3a04e5672c999f876fbb91922a301a3209bf7796
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-03-14 23:17:16 +0700

    manage multipayment

--- #/corelib/acl.php
+++ #/corelib/acl.php
@@ -17,6 +17,11 @@ $__ACL__ = [
     'edit-product-category',
     'delete-product-category',
     
+    'view-multipayment-accounts',
+    'add-multipayment-account',
+    'edit-multipayment-account',
+    'delete-multipayment-account',
+    
     'delete-sales-order',
     
     'delete-purchase-order',
@@ -34,12 +39,16 @@ $__ACL__ = [
     
     'add-product-category',
     'edit-product-category',
+    
+    'view-multipayment-account-list',
   ],
   3 => [
     'add-product-category',
+    
+    'view-multipayment-accounts',
   ],
   4 => [
-    
+    'view-multipayment-accounts',
   ]
 ];
 
@@ -48,4 +57,9 @@ $__ACL__ = $__ACL__[$_SESSION['CURRENT_USER']->groupId];
 function current_user_can($action) {
   global $__ACL__;
   return in_array($action, $__ACL__);
+}
+
+function ensure_current_user_can($doAction) {
+  if (!current_user_can($doAction))
+    exit(render('error/403'));
 }
