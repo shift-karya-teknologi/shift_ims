3646ce2ac66870071105b303273a56e4896b4896
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-03-14 17:12:47 +0700

    menampilkan biaya operasional di dashboard

--- #/handlers/index.php
+++ #/handlers/index.php
@@ -32,10 +32,24 @@ $q->bindValue(2, $shiftEnd->format('Y-m-d H:i:s'));
 $q->execute();
 $data['shiftnet']['total_sales_2'] = $q->fetch(PDO::FETCH_COLUMN);
 
+$q = $db->prepare('select sum(amount) from operational_costs where (dateTime >=? and dateTime<?)');
+$q->bindValue(1, $shiftStart->format('Y-m-d H:i:s'));
+$q->bindValue(2, $shiftMid->format('Y-m-d H:i:s'));
+$q->execute();
+$data['costs']['shift_1'] = $q->fetch(PDO::FETCH_COLUMN);
+
+$q = $db->prepare('select sum(amount) from operational_costs where (dateTime >=? and dateTime<?)');
+$q->bindValue(1, $shiftMid->format('Y-m-d H:i:s'));
+$q->bindValue(2, $shiftEnd->format('Y-m-d H:i:s'));
+$q->execute();
+$data['costs']['shift_2'] = $q->fetch(PDO::FETCH_COLUMN);
+
+$data['stock'] = $db->query('select sum(cost*quantity) from products')->fetch(PDO::FETCH_COLUMN);
+
 render('layout', [
   'title'   => 'Dashboard',
   'sidenav' => render('sidenav', true),
   'content' => render('index', [
-    'data' => $data
+    'data' => $data,
   ], true),
 ]);
