28c1cbf495fb55ef65e605c3d4ca8c8d731702f5
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-03-16 23:46:11 +0700

    - tambahan jenis transaksi komisi dan refund - method pendukung

--- #/corelib/MultiPaymentTransaction.php
+++ #/corelib/MultiPaymentTransaction.php
@@ -5,6 +5,16 @@ class MultiPaymentTransaction
   const Adjustment = 0;
   const TopUp = 1;
   const Sales = 2;
+  const Refund = 3;
+  const Profit = 4;
+  
+  private static $_types = [
+    self::Adjustment => "Penyesuaian",
+    self::TopUp => "Top Up",
+    self::Sales => "Penjualan",
+    self::Refund => "Refund",
+    self::Profit => "Komisi",
+  ];
   
   public $id;
   public $type;
@@ -15,5 +25,18 @@ class MultiPaymentTransaction
   public $userName;
   public $description;
   public $amount;
+  public $salesOrderDetailId;
+  
+  public function getCode() {
+    return '#MPX-' . str_pad($this->id, 6, '0', STR_PAD_LEFT);
+  }
+  
+  public function getTypeName() {
+    return self::$_types[$this->type];
+  }
+  
+  public static function getTypes() {
+    return self::$_types;
+  }
 }
 
