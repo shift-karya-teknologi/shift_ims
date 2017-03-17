0856cf98a5138bc885b3020b835ffab7e63ea272
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-03-17 18:30:06 +0700

    modul kredit

---
+++ #/corelib/CreditAccount.php
@@ -0,0 +1,39 @@
+<?php
+
+class CreditAccount
+{  
+
+  public $id;
+  
+  public $customerName;
+  public $customerAddress;
+  public $customerContact;
+  public $customerId;
+  
+  public $productName;
+  public $productPrice;
+  public $productSerialNumber;
+  
+  public $administrationCost;
+  
+  public $dueDate;
+  
+  public $referralId;
+  public $referralName;
+  
+  public $balance = 0;
+  
+  public $creationDateTime;
+  public $creationUserId;
+  public $creationUsername;
+  
+  public $lastModDateTime;
+  public $lastModUserId;
+  public $lastModUsername;
+  
+  public function getCode() {
+    return '#CA-' . str_pad($this->id, 4, '0', STR_PAD_LEFT);
+  }
+
+}
+
