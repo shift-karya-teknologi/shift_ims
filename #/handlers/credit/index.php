fa1a2700ff0b0b11aabb2be05b1bfedb85fcbb29
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-03-18 19:52:21 +0700

    dashboard menampilkan total piutang

--- #/handlers/credit/index.php
+++ #/handlers/credit/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ensure_current_user_can('open-credit-app');
+
+$total = $db->query('select sum(balance) from credit_accounts')->fetchColumn();
 
 render('credit/dashboard', [
+  'total' => $total
 ]);
--- #/handlers/credit/transaction/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$items = [];
-
-render('credit/transaction/list', [
-  'items' => $items,
-]);
