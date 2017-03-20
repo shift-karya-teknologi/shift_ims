83c44c5af2f826f01bad53f1befe46e4e8c51745
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-03-20 14:13:30 +0700

    - dashboard administrator - tambah menu kategori transaksi keuangan

--- #/handlers/finance/index.php
+++ #/handlers/finance/index.php
@@ -1,4 +1,15 @@
 <?php
 
-header('Location: ./operational-cost/');
-exit;
+ensure_current_user_can('open-finance-app');
+
+$data = [];
+
+if ($_SESSION['CURRENT_USER']->groupId == 1) {
+  $data['cash']['accounts'] = $db->query('select * from finance_accounts order by name asc')->fetchAll(PDO::FETCH_OBJ);
+  $data['cash']['total']    = $db->query('select sum(balance) from finance_accounts')->fetchColumn();
+}
+else {
+  $data['cash']['account'] = $db->query("select * from finance_accounts where id=$cfg[store_account_id]")->fetchObject();
+}
+
+render('finance/index', $data);
