66abbc605ba78e0fdb6c1f9bf4d14790912d25eb
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-03-28 18:07:56 +0700

    laporan

--- #/handlers/finance/report/monthly-by-category.php
+++ #/handlers/finance/report/monthly-by-category.php
@@ -1,21 +1,26 @@
 <?php
 
 $items = [];
-$q = $db->query('select * from finance_transactions');
+$accountId = 2;
+$q = $db->query("select t.*, c.name categoryName from finance_transactions t"
+  . " left join finance_transaction_categories c on c.id=t.categoryId"
+  . " where t.accountId=$accountId"
+  . " and t.amount < 0");
+
+$total = 0;
 while ($item = $q->fetchObject()) {
-  $items[] = $item;
+  if (empty($item->categoryName))
+    $item->categoryName = 'Tanpa Kategori';
+  else if ($item->categoryName == 'Pengeluaran Usaha'
+    || $item->categoryName == 'Utang Piutang'
+    || $item->categoryName == 'Proyek')
+    continue;
+  
+  if (!isset($items[$item->categoryName]))
+    $items[$item->categoryName] = 0;
+  
+  $items[$item->categoryName] += abs($item->amount);
+  $total += abs($item->amount);
 }
 
-
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <?= render('head') ?>
-  </head>
-  <body>
-    <?php foreach ($items as $item): ?>
-      
-    <?php endforeach ?>
-  </body>
-</html>
+render('finance/report/monthly-by-category', ['items' => $items, 'date' => strtotime(date('Y-m-d'))]);
